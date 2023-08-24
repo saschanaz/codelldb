49ae8a6d19732ce6e81c15eb7427a484b6c42efb
Mike Wallio <michael.wallio@outlook.com>
2023-08-24 10:33:20 -0400

    Add win32-arm64 to supported platforms
    Co-authored-by: Kagami Sascha Rosylight <krosylight@proton.me>

---
+++ cmake/toolchain-aarch64-windows-gnu.cmake
@@ -0,0 +1,3 @@
+set(LLVM_TRIPLE aarch64-pc-windows-gnu)
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
---
+++ cmake/toolchain-aarch64-windows-msvc.cmake
@@ -0,0 +1,3 @@
+set(LLVM_TRIPLE aarch64-pc-windows-msvc)
+set(CMAKE_C_COMPILER cl)
+set(CMAKE_CXX_COMPILER cl)
