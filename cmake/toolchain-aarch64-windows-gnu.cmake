06558fa62af23a74ea5a2509b69537b5d620fd93
Kagami Sascha Rosylight <krosylight@proton.me>
2026-08-01 18:15:21 +0200

    use cmd to build for windows

--- cmake/toolchain-aarch64-windows-gnu.cmake
+++
@@ -1,3 +0,0 @@
-set(LLVM_TRIPLE aarch64-pc-windows-gnu)
-set(CMAKE_C_COMPILER gcc)
-set(CMAKE_CXX_COMPILER g++)
---
+++ cmake/toolchain-aarch64-windows-gnullvm.cmake
@@ -0,0 +1,3 @@
+set(LLVM_TRIPLE aarch64-pc-windows-gnullvm)
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
